ca910ef73ff1b2401475cf1f0edaf70803916640
Jiaqi Ye <jiaqi.ye24@gmail.com>
2022-08-12 15:22:37 -0400

    Collapsable box can collapse

--- collapsableBox.py
+++ collapsableBox.py
@@ -1,5 +1,6 @@
+from PyQt5.QtCore import QPropertyAnimation, QParallelAnimationGroup, QAbstractAnimation, QSize
 from PyQt5.QtWidgets import (
-    QVBoxLayout, QPushButton, QTextEdit, QWidget
+    QVBoxLayout, QPushButton, QTextEdit, QWidget, QSizePolicy
 )
 
 
@@ -7,18 +8,36 @@ class CollapsableBox(QWidget):
     def __init__(self, text):
         # Set up
         super().__init__()
-        layout = QVBoxLayout()
-        button = QPushButton(text)
+
         self.textEdit = QTextEdit()
-        self.textEdit.setMinimumHeight(500)
-        layout.addWidget(button)
+        self.textEdit.setMaximumHeight(0)
+        self.textEdit.setMinimumHeight(0)
+        self.textEdit.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        self.animation_group = QParallelAnimationGroup(self)
+
+        layout = QVBoxLayout(self)
+        self.button = QPushButton(text)
+        self.button.setCheckable(True)
+        self.button.setChecked(False)
+        layout.addWidget(self.button)
         layout.addWidget(self.textEdit)
 
         # Animation
+        content_height = self.textEdit.sizeHint().height()
+        self.content_animation = QPropertyAnimation(self.textEdit, b"maximumHeight")
+        self.content_animation.setDuration(500)
+        self.content_animation.setStartValue(0)
+        self.content_animation.setEndValue(content_height)
 
-        button.clicked.connect(self.button_was_clicked)
+        self.button.clicked.connect(self.button_was_clicked)
 
         self.setLayout(layout)
 
     def button_was_clicked(self):
-        pass
+        checked = self.button.isChecked()
+        if checked:
+            self.content_animation.setDirection(QAbstractAnimation.Forward)
+        else:
+            self.content_animation.setDirection(QAbstractAnimation.Backward)
+        self.content_animation.start()
+
