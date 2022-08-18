22af96f86a306ea4deb8adff5f4dec1d48eba045
Jiaqi Ye <jiaqi.ye24@gmail.com>
2022-08-18 12:00:23 -0400

    Remove button successfully removes the checked work summaries

--- workSummary.py
+++ workSummary.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QCheckBox
+
 
 class WorkSummary(QWidget):
     def __init__(self, parent, title, tags, description):
@@ -9,9 +10,15 @@ class WorkSummary(QWidget):
         self.setMinimumSize(QSize(500, 400))
         self.setMaximumSize(QSize(500, 400))
 
+        self.topLayout = QHBoxLayout()
+
         self.titleButton = QPushButton(title)
         self.titleButton.setStyleSheet("background-color:yellow;border:1px solid black;font:bold 20px")
         self.titleButton.setMaximumSize(QSize(400, 50))
+        self.topLayout.addWidget(self.titleButton)
+        self.checkBox = QCheckBox()
+        self.checkBox.hide()
+        self.topLayout.addWidget(self.checkBox)
         self.tagLabel = QLabel(tags)
         self.tagLabel.setStyleSheet("background-color:white;border:1px solid black;font:14px")
         self.descriptionLabel = QLabel(description)
@@ -19,7 +26,7 @@ class WorkSummary(QWidget):
 
         self.mainLayout = QVBoxLayout()
 
-        self.mainLayout.addWidget(self.titleButton)
+        self.mainLayout.addLayout(self.topLayout)
         self.mainLayout.addWidget(self.tagLabel)
         self.mainLayout.addWidget(self.descriptionLabel)
         self.setLayout(self.mainLayout)
@@ -29,4 +36,12 @@ class WorkSummary(QWidget):
     def on_title_clicked(self):
         self.my_parent.open_work(self.titleButton.text())
 
+    def show_checkbox(self):
+        self.checkBox.show()
+
+    def is_checked(self):
+        return self.checkBox.isChecked()
+
+    def get_title(self):
+        return self.titleButton.text()
 
--- worksWindow.py
+++ worksWindow.py
@@ -3,7 +3,7 @@ from PyQt5.QtCore import QSize
 import customDialog
 import workCreationWidget
 import workSummary
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QScrollArea, QWidget, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QScrollArea, QWidget, QPushButton, QLabel
 
 import writingWindow
 
@@ -20,6 +20,10 @@ class WorksWindow(QMainWindow):
         self.layout = QVBoxLayout()
         self.createButton = QPushButton("Create New Work")
         self.layout.addWidget(self.createButton)
+        self.removeButton = QPushButton("Remove Works")
+        self.removeButton.setCheckable(True)
+        self.removeButton.setChecked(False)
+        self.layout.addWidget(self.removeButton)
 
         # Finish set up
         widget = QWidget()
@@ -30,6 +34,7 @@ class WorksWindow(QMainWindow):
         self.setCentralWidget(self.scroll)
 
         self.createButton.clicked.connect(self.on_create_clicked)
+        self.removeButton.clicked.connect(self.on_remove_clicked)
 
     def on_create_clicked(self):
         workCreation = workCreationWidget.WorkCreationWidget()
@@ -42,7 +47,27 @@ class WorksWindow(QMainWindow):
         work = workSummary.WorkSummary(self, widget.get_title(), widget.get_tags(), widget.get_description())
         self.layout.addWidget(work)
         workWriting = writingWindow.WritingWindow(widget.get_title())
-        self.works[widget.get_title()] = workWriting
+        self.works[widget.get_title()] = [work, workWriting]
 
     def open_work(self, title):
-        work = self.works[title].show()
+        self.works[title][1].show()
+
+    def on_remove_clicked(self):
+        if self.removeButton.isChecked():
+            for key, value in self.works.items():
+                value[0].show_checkbox()
+        else:
+            dlg = customDialog.CustomDialog(
+                self, "Remove Works", QSize(300, 100), QLabel("Are you sure you want to remove these works?\nTHIS IS NOT REVERSIBLE."), self.on_remove_ok, self.revert_remove
+            )
+            dlg.exec()
+
+    def on_remove_ok(self):
+        for title in list(self.works):
+            summary = self.works[title][0]
+            if summary.is_checked():
+                self.layout.removeWidget(summary)
+                self.works.pop(title)
+
+    def revert_remove(self):
+        pass
