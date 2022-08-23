037711082d5cf229929483535098787de7ee4d0a
Jiaqi Ye <jiaqi.ye24@gmail.com>
2022-08-23 11:35:06 -0400

    Integrated removableItemsHolder into workPage (but without remove function)

--- removableItemsHolder.py
+++ removableItemsHolder.py
@@ -2,6 +2,9 @@ from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import QGroupBox, QVBoxLayout, QLabel
 
 import customDialog
+import workCreationWidget
+import workSummary
+
 
 class RemovableItemsHolder(QGroupBox):
     def __init__(self, remove_button, part_creation_widget, part_summary, part):
@@ -13,7 +16,7 @@ class RemovableItemsHolder(QGroupBox):
         self.main_layout = QVBoxLayout()
         self.setLayout(self.main_layout)
 
-        self.works = {}
+        self.parts = {}
 
     def on_create_clicked(self):
         helper_widget = self.part_creation_widget()
@@ -23,17 +26,23 @@ class RemovableItemsHolder(QGroupBox):
         dlg.exec()
 
     def on_create_ok(self, widget):
-        my_part_summary = self.part_summary(self, widget.get_title(), widget.get_tags(), widget.get_description())
+        my_part_summary = None
+        my_part = None
+        if isinstance(widget, workCreationWidget.WorkCreationWidget):
+            my_part_summary = self.part_summary(self, widget.get_title(), widget.get_tags(), widget.get_description())
+            my_part = self.part(widget.get_title(), widget.get_tags(), widget.get_description())
+        else:
+            my_part_summary = self.part_summary(self, widget.get_title(), widget.get_description(), len(self.parts) + 1)
+            my_part = self.part(widget.get_title())
         self.main_layout.addWidget(my_part_summary)
-        my_part = self.part(widget.get_title(), widget.get_tags(), widget.get_description())
-        self.works[widget.get_title()] = [my_part_summary, my_part]
+        self.parts[widget.get_title()] = [my_part_summary, my_part]
 
     def toggle_all_checkboxes(self):
-        for key, value in self.works.items():
+        for key, value in self.parts.items():
             value[0].toggle_checkbox_visible()
 
     def open_part(self, title):
-        self.works[title][1].show()
+        self.parts[title][1].show()
 
     def on_remove_clicked(self):
         if self.remove_button.isChecked():
@@ -47,11 +56,11 @@ class RemovableItemsHolder(QGroupBox):
             dlg.exec()
 
     def on_remove_ok(self):
-        for title in list(self.works):
+        for title in list(self.parts):
             summary = self.works[title][0]
             if summary.is_checked():
                 self.main_layout.removeWidget(summary)
-                self.works.pop(title)
+                self.parts.pop(title)
         self.toggle_all_checkboxes()
 
     def on_enter_edit_ok(self):
